f663c54fc336073086df58197cc335a30453e85f
alexandro458 <alejandrovega10.10@gmail.com>
2024-01-02 14:51:18 +0100

    rendering a quad

--- main.py
+++ main.py
@@ -2,11 +2,6 @@ from settings import *
 import moderngl as mgl
 import pygame as pg
 import sys
-from shader_program import ShaderProgram
-from scene import Scene
-from player import Player
-from textures import Textures
-
 
 class VoxelEngine:
     def __init__(self):
@@ -33,29 +28,35 @@ class VoxelEngine:
         self.on_init()
 
     def on_init(self):
-        self.textures = Textures(self)
-        self.player = Player(self)
-        self.shader_program = ShaderProgram(self)
-        self.scene = Scene(self)
-
-    def update(self):
-        self.player.update()
-        self.shader_program.update()
-        self.scene.update()
-
-        self.delta_time = self.clock.tick()
-        self.time = pg.time.get_ticks() * 0.001
-        pg.display.set_caption(f'{self.clock.get_fps() :.0f}')
-
-    def render(self):
-        self.ctx.clear(color=BG_COLOR)
-        self.scene.render()
-        pg.display.flip()
+        vertices = np.array([-0.5, -0.5, 0.5, -0.5, 0.5, 0.5, -0.5, 0.5], dtype='f4')
+        self.vbo = self.ctx.buffer(vertices)
+        self.vao = self.ctx.simple_vertex_array(self.get_program('quad'), self.vbo, 'in_vert')
 
     def handle_events(self):
         for event in pg.event.get():
             if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
                 self.is_running = False
+
+    def update(self):
+        pass
+
+    def render(self):
+        self.ctx.clear()
+
+        self.vao.render(mgl.TRIANGLE_FAN)
+
+        pg.display.flip()
+        pass
+
+    def get_program(self, shader_name):
+        with open(f'shaders/{shader_name}.vert') as file:
+            vertex_shader = file.read()
+
+        with open(f'shaders/{shader_name}.frag') as file:
+            fragment_shader = file.read()
+
+        program = self.ctx.program(vertex_shader=vertex_shader, fragment_shader=fragment_shader)
+        return program
 
     def run(self):
         while self.is_running:
